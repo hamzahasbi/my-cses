0dfcb656433f4664e8ca2a04cdd119fe2ab376e8
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-12-19 00:16:35 +0100

    Update: Ferris Wheel problem

---
+++ Sorting and Searching/Ferris_Wheel.cpp
@@ -0,0 +1,62 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("IO/Input.in", "r", stdin);
+  #endif
+}
+int main() {
+    // auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    int n;
+    long long x;
+    cin >> n >> x;
+    vector<long long>p(n + 1, 0);
+    for(int i = 0; i < n; ++i) {
+        cin >> p[i];
+    }
+    sort(p.begin(), p.end() - 1);
+
+    int ans = 0;
+    int i = 0, j = n - 1;
+    while(i <=j ) {
+
+        if (p[i] + p[j] <= x) {
+            // cout << p[i] << " " << p[j] << endl;
+            ans++;
+            i += 1;
+            j -= 1;
+        }
+        else {
+            // cout << p[i] << endl;
+            ans++;
+            j--;
+
+        }
+    }
+    cout << ans << endl;
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    // cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
