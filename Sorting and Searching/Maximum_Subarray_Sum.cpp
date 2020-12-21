3ab1cfb0989f2ac14e08db9483aaed490c2416ec
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-12-21 22:14:58 +0100

    feat: max subarray

---
+++ Sorting and Searching/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,54 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("IO/Input.in", "r", stdin);
+  #endif
+}
+
+long long maxSubArraySum(vector<long long> a, int size) 
+{ 
+   long long max_so_far = a[0]; 
+   long long curr_max = a[0]; 
+  
+   for (int i = 1; i < size; i++) { 
+        curr_max = max(a[i], curr_max + a[i]); 
+        max_so_far = max(max_so_far, curr_max); 
+   } 
+   return max_so_far; 
+} 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    
+
+    int n;
+    cin >> n;
+    vector<long long>a(n);
+
+    for(int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    
+    cout << maxSubArraySum(a, n) << endl;
+    return 0;
+}
