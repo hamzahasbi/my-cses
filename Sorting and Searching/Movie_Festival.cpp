97d7ae943a5e583a06a4a34290ca80b5f04c55d8
Hamza Hasbi <hamza.hasbi@gmail.com>
2020-12-26 23:16:56 +0100

    Add: sticks & movie festival

---
+++ Sorting and Searching/Movie_Festival.cpp
@@ -0,0 +1,57 @@
+/*
+ Author: Hamza Hasbi
+ @Copyrights: __Morty
+*/
+#include<bits/stdc++.h>
+#ifdef ONLINE_JUDGE
+ #pragma GCC optimize("O3")
+#endif
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+#define uld unsigned long double
+#define ud unsigned double
+#define uf unsigned float
+#define pi 2 * acos(0.0)
+#define module cin.ignore()
+//inline long long lcm(ll a,ll b) {return a * b / __gcd(a,b);}
+//inline long long gcd(ll a,ll b) {return 1LL * b == 0 ? a : gcd(1LL*b, a * 1LL % b * 1LL);}
+inline bool IsPowerOfTwo (ull x){ return (x != 0) && ((x & (x - 1)) == 0);}
+using namespace std;
+void READ() {
+ #ifndef ONLINE_JUDGE
+   freopen("IO/Input.in", "r", stdin);
+  #endif
+}
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    READ();
+    
+
+    int n;
+    cin >> n;
+    vector<pair<long long, long long>>a(n);
+    long long curr_end = 0, total = 0;
+    for(int i = 0; i < n; ++i) {
+        cin >> a[i].second >> a[i].first;
+    }
+ 
+    sort(a.begin(), a.end());
+
+    for(int i = 0; i < n; ++i) {
+
+       if (a[i].second >= curr_end)
+		{
+			curr_end = a[i].first;
+			total++;
+		}
+    }
+
+    cout << total << endl;
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    // cout << "Finished in " << duration << " ms" << endl;
+    return 0;
+}
